10bb0dbb1dd9a6016b39196aac0e53b352bbe0cf
arhizsx <arhizsx@gmail.com>
2024-04-01 18:54:27 +0800

    Global V2 - Updates in UI

--- resources/views/record-update-chapter-registered.blade.php
+++ resources/views/record-update-chapter-registered.blade.php
@@ -17,10 +17,10 @@
             <small>Chapters you registered in the system</small>
         </div>
         <div class="col-xl-9 mb-3 pe-4 ps-4">
-                <?php
+                @php
                     $chapters = DB::table("chapters")
                                 ->get();
-                ?>
+                @endphp
                 @if( count($chapters) > 0 )
                     @foreach($chapters as $chapter)
                     <div class="fields_box mb-3">
